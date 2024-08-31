1f91032ed6cbc09b05b55ef62afcdd7d58770cf0
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-08-31 18:17:47 +0900

    in progress

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,10 +1,23 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const codeString = "(num) => num + 1";
+  const codeString = `(num) => num + 1
+(num) => num + 1
+(num) => num + 1
+(num) => num + 1
+(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1(num) => num + 1
+(num) => num + 1
+  `;
   return (
-    <SyntaxHighlighter language="javascript" style={docco}>
+    <SyntaxHighlighter
+      className={styles.component}
+      language="javascript"
+      style={docco}
+      wrapLines
+      lineProps={(n) => ({ className: "specialline" })}
+    >
       {codeString}
     </SyntaxHighlighter>
   );
